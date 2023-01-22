e49c19f607732349c88b97a451d5c94a70e3386c
aleksey.repin <jesterun22@gmail.com>
2023-01-22 12:58:56 +0300

    done

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
-import os
 
 with open('moscow_books.csv', 'w', encoding='utf-8-sig', newline='') as file:
     writer = csv.writer(file, delimiter=';')
     writer.writerow([
-        'Автор', 'Книга', 'Цена'])
+        'Автор', 'Книга', 'Цена', 'Обложка'])
 
-for j in range(1, 4):
+for j in range(1, 357):
+
+    def percentage(part, whole):
+        percentage = 100 * float(part) / float(whole)
+        return round(percentage, 1)
 
     url = f'https://www.moscowbooks.ru/books/fiction/?page={j}'
     response = requests.get(url=url)
@@ -18,25 +21,16 @@ for j in range(1, 4):
     price = [name.text.strip() for name in soup.find_all('div', class_='book-preview__price')]
     author = [name.text for name in soup.find_all('a', class_='author-name')]
     title = [name.text.strip('"').replace('"', '') for name in soup.find_all('a', class_='book-preview__title-link')]
-    img = [name['src'] for name in soup.find_all('img', class_='book-preview__img')]
 
-    for title_name, author_name, book_price, img_url in zip(title, author, price, img):
+    for title_name, author_name, book_price in zip(title, author, price):
         flatten = author_name, title_name, book_price
-
-        if os.path.exists(f'/home/xennen/scratchbooks/books/{author_name}') == False:
-            os.mkdir(f'/home/xennen/scratchbooks/books/{author_name}')
-        os.mkdir(f'/home/xennen/scratchbooks/books/{author_name}/{title_name}')
-        file2 = open(f'/home/xennen/scratchbooks/books/{author_name}/{title_name}/{title_name}, {book_price}.txt', 'x', encoding='utf-8')
-
-        image = requests.get(url=f'https://www.moscowbooks.ru/{img_url}')
-        with open(f'/home/xennen/scratchbooks/books/{author_name}/{title_name}/{title_name}.jpeg', 'wb') as file:
-            file.write(response.content)
 
         with open('moscow_books.csv', 'a', newline='', encoding='utf-8-sig') as file:
             writer = csv.writer(file, delimiter=';')
             writer.writerow(flatten)
 
     file.close()
-    print(f'Страница {j} готова')
+
+    print(percentage(j, 357), '%', sep='')
 
 
